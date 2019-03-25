3055f90a41da1c2171c6fe923f335cf77f9f373a
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-25 20:17:36 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artist.names
+    self.artists.names
 
   end
 end
